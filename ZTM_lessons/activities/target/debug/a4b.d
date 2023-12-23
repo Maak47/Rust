e2e6c5a2668c6e096f8cc4118feaaf68017c192c
Maak47 <oshaf.khan10@gmail.com>
2023-12-23 10:46:29 +0530

    activity 4b done

---
+++ ZTM_lessons/activities/target/debug/a4b.d
@@ -0,0 +1,1 @@
+C:\Development\Projects\Rust\ZTM_lessons\activities\target\debug\a4b.exe: C:\Development\Projects\Rust\ZTM_lessons\activities\src\bin\a4b.rs
---
+++ ZTM_lessons/activities/target/debug/deps/a4b.d
@@ -0,0 +1,5 @@
+C:\Development\Projects\Rust\ZTM_lessons\activities\target\debug\deps\a4b.exe: src\bin\a4b.rs
+
+C:\Development\Projects\Rust\ZTM_lessons\activities\target\debug\deps\a4b.d: src\bin\a4b.rs
+
+src\bin\a4b.rs:
