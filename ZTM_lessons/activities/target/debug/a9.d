11291e52c80a2cc89d14f67cef64f94422e176f1
Maak47 <oshaf.khan10@gmail.com>
2023-12-25 08:55:48 +0530

    activity 9 done;

---
+++ ZTM_lessons/activities/target/debug/a9.d
@@ -0,0 +1,1 @@
+C:\Development\Projects\Rust\ZTM_lessons\activities\target\debug\a9.exe: C:\Development\Projects\Rust\ZTM_lessons\activities\src\bin\a9.rs
---
+++ ZTM_lessons/activities/target/debug/deps/a9.d
@@ -0,0 +1,5 @@
+C:\Development\Projects\Rust\ZTM_lessons\activities\target\debug\deps\a9.exe: src\bin\a9.rs
+
+C:\Development\Projects\Rust\ZTM_lessons\activities\target\debug\deps\a9.d: src\bin\a9.rs
+
+src\bin\a9.rs:
