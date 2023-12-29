71bbeef08d7cc0f6ccc25e0944e71819c42ececa
Maak47 <oshaf.khan10@gmail.com>
2023-12-29 12:31:30 +0530

    activity 18 done

---
+++ ZTM_lessons/activities/target/debug/a18.d
@@ -0,0 +1,1 @@
+C:\Development\Projects\Rust\ZTM_lessons\activities\target\debug\a18.exe: C:\Development\Projects\Rust\ZTM_lessons\activities\src\bin\a18.rs
---
+++ ZTM_lessons/activities/target/debug/deps/a18.d
@@ -0,0 +1,5 @@
+C:\Development\Projects\Rust\ZTM_lessons\activities\target\debug\deps\a18.exe: src\bin\a18.rs
+
+C:\Development\Projects\Rust\ZTM_lessons\activities\target\debug\deps\a18.d: src\bin\a18.rs
+
+src\bin\a18.rs:
