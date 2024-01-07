21dd49fd869d2adc934af7d17b49255097e80f64
Maak47 <oshaf.khan10@gmail.com>
2024-01-07 15:54:51 +0530

    activity revision, a12 done

--- ZTM_lessons/activities/target/debug/deps/a1.d
+++ ZTM_lessons/activities/target/debug/deps/a1.d
@@ -1,5 +1,5 @@
-C:\Development\Projects\Rust\ZTM_lessons\day_2\activities\target\debug\deps\a1.exe: src\bin\a1.rs
+C:\Development\Projects\Rust\ZTM_lessons\activities\target\debug\deps\a1.exe: src\bin\a1.rs
 
-C:\Development\Projects\Rust\ZTM_lessons\day_2\activities\target\debug\deps\a1.d: src\bin\a1.rs
+C:\Development\Projects\Rust\ZTM_lessons\activities\target\debug\deps\a1.d: src\bin\a1.rs
 
 src\bin\a1.rs:
---
+++ ZTM_lessons/day_9/src/bin/revision.rs
@@ -0,0 +1,290 @@
+// REVISION
+// // 1
+// fn first_name() {
+//     println!("Maak");
+// }
+
+// fn last_name() {
+//     println!("47");
+// }
+
+// fn main() {
+//     first_name();
+//     last_name();
+// }
+
+
+// // 2
+// fn add(a: i32, b: i32) -> i32 {
+//     a + b
+// }
+
+// fn display_result(result: i32){
+//     println!("{:?}", result);
+// }
+
+// fn main(){
+//     let result = add(2, 3);
+//     display_result(result);
+// }
+// // 3a
+// fn main() {
+//     let message = false;
+//     if message == true {
+//         println!("hello");
+//     } else {
+//         println!("goodbye");
+//     }
+// }
+
+// // 3b
+// fn main() {
+//     let variable = 4;
+//     if variable < 5 {
+//         println!("<5");
+//     } else if variable == 5 {
+//         println!("=5");
+//     } else {
+//         println!(">5");
+//     }
+// }
+
+// // 4a 
+// fn main() {
+//     let condition = true;
+
+//     match condition {
+//         true => println!("it's true"),
+//         false => println!("it's false"),
+//     }
+// }
+
+// // 4b 
+// fn main() {
+//     let integer = 4;
+//     match integer {
+//         1 => println!("one"),
+//         2 => println!("two"),
+//         3 => println!("three"),
+//         _ => println!("other")
+//     }
+// }
+
+// // 5 
+// fn main() {
+//     let mut value = 1;
+//     loop {
+//         println!("{:?}", value);
+   
+//         if value == 4{
+//             break;
+//         }
+//         value += 1;
+//     }
+    
+// }
+
+// // 6
+// fn main() {
+//     let mut value = 5;
+
+//     while value >= 1{
+//         println!("{:?}", value);
+//         value -= 1;
+//     }
+//     println!("done!");
+// }
+
+// // 7
+// enum Color {
+//     Red,
+//     Orange,
+//     Yellow,
+//     Green,
+//     Blue,
+//     Indigo,
+//     Violet,
+// }
+
+
+// fn print_color(color: Color){
+//     match color {
+//         Color::Red => println!("red"),
+//         Color::Orange => println!("orange"),
+//         Color::Yellow => println!("yellow"),
+//         Color::Green => println!("green"),
+//         Color::Blue => println!("blue"),
+//         Color::Indigo => println!("indigo"),
+//         Color::Violet => println!("violet"),
+//     }
+// }
+
+// fn main(){
+//     print_color(Color::Red);
+// }
+
+// // 8 
+// enum Flavors {
+//     Sparkling,
+//     Cold,
+//     Hot,
+// }
+
+
+// struct Drink {
+//     flavor: Flavors,
+//     quantity: f64,
+// }
+
+
+// fn print_information(drink: Drink) {
+//     match drink.flavor {
+//         Flavors::Sparkling => println!("flavor: sparkling"),
+//         Flavors::Cold => println!("flavor: cold"),
+//         Flavors::Hot => println!("flavor: hot"),
+//     }
+
+//     println!("quantity: {:?}", drink.quantity);
+// }
+
+
+// fn main() {
+//     let cold_drink = Drink {
+//         flavor: Flavors::Cold,
+//         quantity: 500.0
+//     };
+//     print_information(cold_drink);
+// }
+
+// // 9
+// fn coords() -> (i32, i32) {
+//     (2, 4)
+// }
+
+// fn main() {
+//     let (_x, y) = coords(); //destructured
+//     if y > 5 {
+//         println!("{:?} greater than 5", y);
+//     } else if y == 5 {
+//         println!("{:?} equal to 5", y);
+//     } else {
+//         println!("{:?} smaller than 5", y);
+//     }
+// }
+
+// // 10
+// fn print_condition(is_gt_100: bool) {
+//     match is_gt_100 {
+//         true => println!("its big"),
+//         false => println!("its small")
+//     }
+// }
+
+// fn main() {
+//     let variable = 234;
+//     let is_gt_100 = variable > 100;
+//     print_condition(is_gt_100);
+// }
+
+// // 11
+// struct GroceryItem {
+//     quantity: i32,
+//     id: i32,
+// }
+
+// fn display_quantity(item: &GroceryItem) {
+//     println!("Quantity: {:?}", item.quantity);
+// }
+
+// fn display_id(item: &GroceryItem){
+//     println!("ID: {:?}", item.id)
+// }
+
+// fn main() {
+//     let carrot = GroceryItem{
+//         quantity: 21,
+//         id: 10023,
+//     };
+   
+//     display_quantity(&carrot);
+//     display_id(&carrot);
+// }
+
+
+// // 12
+// enum BoxColor {
+//     Brown,
+//     DampGreen,
+//     SandalWood,
+// }
+
+// impl BoxColor{
+//     fn print(&self) {
+//         match self {
+//             BoxColor::Brown => println!("color: brown"),
+//             BoxColor::DampGreen => println!("color: dampgreen"),
+//             BoxColor::SandalWood => println!("color: sandalwood"),
+//         }
+//     }
+// }
+
+// struct Dimensions {
+//     height: i32,
+//     width: i32,
+//     depth: i32,
+// }
+
+// impl Dimensions{
+//     fn print(&self) {
+//         println!("height: {:?}", self.height);
+//         println!("width: {:?}", self.width);
+//         println!("depth: {:?}", self.depth);
+//     }
+// }
+
+// struct Box{
+//     color: BoxColor,
+//     weight: i32,
+//     dimensions: Dimensions
+// }
+
+// impl Box {
+//     fn create_box(weight: i32, color: BoxColor, dimensions: Dimensions) -> Self{
+//     Self {
+//         weight,
+//         color,
+//         dimensions,
+//     }
+//     }
+
+//     fn print_box(&self) {
+//         self.color.print();
+//       self.dimensions.print();
+//       println!("weight: {:?}", self.weight);
+//     }
+// }
+
+
+// fn main() {
+// let machine_box_dimensions = Dimensions{
+//     height: 23,
+//     width: 12,
+//     depth: 17
+// };
+// let machine_box = Box::create_box(12, BoxColor::DampGreen, machine_box_dimensions);
+
+// machine_box.print_box();
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
