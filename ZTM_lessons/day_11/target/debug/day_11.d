6e0d05f354150d1fbe41d5b2c9da7c9a73ce1d24
Maak47 <oshaf.khan10@gmail.com>
2024-01-09 08:12:23 +0530

    day_11 init

---
+++ ZTM_lessons/day_11/target/debug/day_11.d
@@ -0,0 +1,1 @@
+C:\Development\Projects\Rust\ZTM_lessons\day_11\target\debug\day_11.exe: C:\Development\Projects\Rust\ZTM_lessons\day_11\src\main.rs
---
+++ ZTM_lessons/day_11/target/debug/deps/day_11.d
@@ -0,0 +1,5 @@
+C:\Development\Projects\Rust\ZTM_lessons\day_11\target\debug\deps\day_11.exe: src\main.rs
+
+C:\Development\Projects\Rust\ZTM_lessons\day_11\target\debug\deps\day_11.d: src\main.rs
+
+src\main.rs:
