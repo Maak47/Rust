98e0c89405c8c5c056169a908c3ba4b6e766b94a
Maak47 <oshaf.khan10@gmail.com>
2024-01-10 16:58:28 +0530

    activity 21 done

---
+++ ZTM_lessons/activities/target/debug/deps/a21.d
@@ -0,0 +1,5 @@
+C:\Development\Projects\Rust\ZTM_lessons\activities\target\debug\deps\a21.exe: src\bin\a21.rs
+
+C:\Development\Projects\Rust\ZTM_lessons\activities\target\debug\deps\a21.d: src\bin\a21.rs
+
+src\bin\a21.rs:
